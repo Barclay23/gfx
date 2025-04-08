0fa9ded9f80b14f25114b312e246438e3bf31f35
Barclay23 <igor.lp@wp.pl>
2025-04-08 13:01:12 +0200

    camera finished

--- kamera/main.py
+++ kamera/main.py
@@ -25,15 +25,13 @@ def wczytaj_dane_z_pliku(plik):
     return zbiory_punktow
 
 
-def przekszt(zbiory_punktow, ex, ey, ez, theta_x, theta_y, theta_z):
+def przekszt(zbiory_punktow, ex, ey, ez, theta_x, theta_y, theta_z, zoom):
     wynik = []
 
-    # Konwersja kątów do radianów
     tx = np.radians(theta_x)
     ty = np.radians(theta_y)
     tz = np.radians(theta_z)
 
-    # Macierze rotacji
     Rx = np.array([
         [1, 0, 0],
         [0, np.cos(tx), np.sin(tx)],
@@ -52,51 +50,59 @@ def przekszt(zbiory_punktow, ex, ey, ez, theta_x, theta_y, theta_z):
         [0, 0, 1]
     ])
 
-    # Macierz transformacji kamery (kolejność rotacji: ZYX)
+    S = np.array([
+        [zoom, 0, 0],
+        [0, zoom, 0],
+        [0, 0, 1]
+    ])
+
     R = Rx @ Ry @ Rz
 
-    # Pozycja kamery
     C = np.array([ex, ey, ez])
 
+    z_clip = 0.01
+
     for zbior in zbiory_punktow:
-        aktualny_zbior = []
+        transformed = []
+
         for dx, dy, dz in zbior:
-            #print(str(dx)+" "+str(dy)+" "+str(dz))
             a = np.array([dx, dy, dz])
-            d = a + C  # wektor względem kamery
-            d_rot = R @ d  # obrót kamery
+            d = a + C
+            d_rot = S @ (R @ d)
+            transformed.append(d_rot)  # 3D punkt po transformacji
 
-            
+        indeksy = [
+            (0, 1), (1, 2), (2, 3), (3, 0),
+            (4, 5), (5, 6), (6, 7), (7, 4),
+            (0, 4), (1, 5), (2, 6), (3, 7)
+        ]
 
-            if d_rot[2] <= 0:
-                continue
-            d_rot[2] = abs(d_rot[2])
-
-
-            x = d_rot[0] / d_rot[2]
-            y = d_rot[1] / d_rot[2]
-            #print(str(x)+" "+str(y))
-            aktualny_zbior.append((x, y))
-
-        # Tworzymy linie
         proste = []
-        if len(aktualny_zbior) >= 8:
-            proste.extend([
-                (aktualny_zbior[0], aktualny_zbior[1]),
-                (aktualny_zbior[1], aktualny_zbior[2]),
-                (aktualny_zbior[2], aktualny_zbior[3]),
-                (aktualny_zbior[3], aktualny_zbior[0]),
-                (aktualny_zbior[4], aktualny_zbior[5]),
-                (aktualny_zbior[5], aktualny_zbior[6]),
-                (aktualny_zbior[6], aktualny_zbior[7]),
-                (aktualny_zbior[7], aktualny_zbior[4]),
-                (aktualny_zbior[0], aktualny_zbior[4]),
-                (aktualny_zbior[1], aktualny_zbior[5]),
-                (aktualny_zbior[2], aktualny_zbior[6]),
-                (aktualny_zbior[3], aktualny_zbior[7]),
-            ])
-        wynik.append(proste)
 
+        for i1, i2 in indeksy:
+            if i1 >= len(transformed) or i2 >= len(transformed):
+                continue
+
+            p1 = transformed[i1]
+            p2 = transformed[i2]
+            z1, z2 = p1[2], p2[2]
+
+            if z1 <= 0 and z2 <= 0:
+                continue
+
+            if z1 <= z_clip or z2 <= z_clip:
+                t = (z_clip - z1) / (z2 - z1)
+                clip_point = p1 + t * (p2 - p1)
+
+                if z1 < z_clip:
+                    p1 = clip_point
+                else:
+                    p2 = clip_point
+
+            x1, y1 = p1[0] / abs(p1[2]), p1[1] / abs(p1[2])
+            x2, y2 = p2[0] / abs(p2[2]), p2[1] / abs(p2[2])
+            proste.append(((x1, y1), (x2, y2)))
+        wynik.append(proste)
     return wynik
 
 
@@ -105,13 +111,13 @@ class Aplikacja:
         self.master = master
         self.master.title("Wyświetlacz")
         self.original_data = original_data
-        self.ey = 1
-        self.ex = 1
-        self.ez = 1
+        self.ey = 0
+        self.ex = 0
+        self.ez = 0
         self.theta_x = 0
         self.theta_y = 0
         self.theta_z = 0
-        self.indeks = 0
+        self.zoom = 1
 
         self.fig, self.ax = plt.subplots()
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.master)
@@ -133,24 +139,26 @@ class Aplikacja:
         tk.Button(frame, text="ry-", command=self.rydown).grid(row=2, column=8)
         tk.Button(frame, text="rz+", command=self.rzup).grid(row=0, column=9)
         tk.Button(frame, text="rz-", command=self.rzdown).grid(row=2, column=9)
+        tk.Button(frame, text="zoom+", command=self.zoomup).grid(row=0, column=9)
+        tk.Button(frame, text="zoom-", command=self.zoomdown).grid(row=2, column=9)
         
 
         self.odswiez_dane()
         self.rysuj()
 
     def odswiez_dane(self):
-        self.dane = przekszt(self.original_data, self.ex, self.ey, self.ez, self.theta_x, self.theta_y, self.theta_z)
+        self.dane = przekszt(self.original_data, self.ex, self.ey, self.ez, self.theta_x, self.theta_y, self.theta_z, self.zoom)
 
     def rysuj(self):
         self.ax.clear()
-        proste = self.dane[self.indeks]
 
-        for x1, x2 in proste:
-            self.ax.plot([x1[0], x2[0]], [x1[1], x2[1]], color='blue')
+        for bryla in self.dane:
+            for x1, x2 in bryla:
+                self.ax.plot([x1[0], x2[0]], [x1[1], x2[1]], color='blue')
 
-        self.ax.set_xlim(-5, 5)  # Ustaw zakres osi X
+        self.ax.set_xlim(-5, 5)
         self.ax.set_ylim(-5, 5)
-
+        self.ax.set_aspect('equal')
         self.ax.axis('off')
         self.canvas.draw()
 
@@ -211,6 +219,18 @@ class Aplikacja:
 
     def rzdown(self):
         self.theta_z-=15
+        self.odswiez_dane()
+        self.rysuj()
+
+    def zoomup(self):
+        if(self.zoom+0.25!=10):
+            self.zoom+=0.25
+        self.odswiez_dane()
+        self.rysuj()
+
+    def zoomdown(self):
+        if(self.zoom-0.25!=0):
+            self.zoom-=0.25
         self.odswiez_dane()
         self.rysuj()
 
